d3acd7204741399e1bdf99b5cc525fda98c6d179
Arik Waisman <arik.waisman@gmail.com>
2019-03-01 15:06:25 -0500

    Updates

---
+++ precache-manifest.f6768e49f1c080f80294a27b7794e355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1e58da068a08a5217703",
+    "url": "/static/js/main.1e58da06.chunk.js"
+  },
+  {
+    "revision": "e2c636c7557de49f5553",
+    "url": "/static/js/2.e2c636c7.chunk.js"
+  },
+  {
+    "revision": "1e58da068a08a5217703",
+    "url": "/static/css/main.e83e839c.chunk.css"
+  },
+  {
+    "revision": "cdcf1018df649b9f27dbd633bf9e56d3",
+    "url": "/index.html"
+  }
+];
